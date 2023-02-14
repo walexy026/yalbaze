f19acf61e09792f6756859a8889c4c382463b9a5
walexy026 <jao.olaniyi@gmail.com>
2023-02-14 10:11:59 +0100

    form conslut

--- src/Components/ConsultForm/ConsultForm.jsx
+++ src/Components/ConsultForm/ConsultForm.jsx
@@ -3,8 +3,8 @@ import "./ConsultForm.scss";
 export const ConsultForm = () => {
   return (
     <div className="consultForm">
-      <h2>Book a Consultation with us</h2>
       <form className="formConsult" action="">
+        <h2>Book a Consultation with us</h2>
         <label htmlFor=""></label>
         <input type="text" name="" id="" placeholder="Enter your full name" />
         <label htmlFor=""></label>
@@ -15,13 +15,15 @@ export const ConsultForm = () => {
         <input type="text" name="" id="" placeholder="Company Name" />
         <label htmlFor=""></label>
         <input type="text" name="" id="" />
-        <textarea
-          name=""
-          id=""
-          cols="40"
-          rows="15"
-          placeholder="Drop a message....."
-        ></textarea>
+        <div className="txArea">
+          <textarea
+            name=""
+            id=""
+            cols="40"
+            rows="15"
+            placeholder="Drop a message....."
+          ></textarea>
+        </div>
         <button className="btnForm">Send Request</button>
       </form>
     </div>
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import "./Footer";
+import "./Footer.scss";
+import house from "../../Assets/house2.svg";
 import { FiPhoneCall } from "react-icons/fi";
 
 export const Footer = () => {
   return (
     <div className="footer">
+      <hr />
       <ul>
         <li>
+          <img src={house} alt="house" />
           2nd Floor, The Garnet Building, KM14 Lekki Epe Expressway, Lagos,
           Nigeria.
         </li>
         <li>Loyalbaze is almost here.</li>
         <li>
-          <FiPhoneCall />
+          <FiPhoneCall fontSize="1rem" />
           +234 903 618 9485
         </li>
       </ul>
